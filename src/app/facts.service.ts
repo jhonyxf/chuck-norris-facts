9b7dc32146eb5226ab21bca0c640862f135eed55
Nathan Pickard <nathan.pickard87@gmail.com>
2019-05-07 21:59:07 -0700

    Fact template updated with vertical alignment

--- src/app/facts.service.ts
+++ src/app/facts.service.ts
@@ -4,11 +4,12 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class FactsService {
 
   constructor(private httpClient: HttpClient) { }
 
   getChuckNorrisFact() {
-    return this.httpClient.get('https://api.chucknorris.io/jokes/random');
+    return this.httpClient.get('https://api.chucknorris.io/jokes/random?category=science');
   }
 }
